8de96248bda427a24cd94e0a40284da86fc413c0
EC2 Default User <ec2-user@ip-172-31-4-178.ap-northeast-1.compute.internal>
2023-02-27 15:37:19 +0000

    [Finish]

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-shop = Shop.create!(
-  [
-    {name: "東京店", email: 'aaa@aaa', password: "123456"}
-  ]
-)
+# shop = Shop.create!(
+#   [
+#     {name: "東京店", email: 'aaa@aaa', password: "123456"}
+#   ]
+# )
 
-date = Date.new(2022/1/1)
-count = 0
-while count < 500 do
-    okane = Random.rand(5000)
-    sales = Sale.create!(
-      [
-        {proceed: okane, shop_id: 1, created_at: date.since(count.days)}
-      ]
-    )
-    count += 1
-end
+# date = Date.new(2022/1/1)
+# count = 0
+# while count < 500 do
+#     okane = Random.rand(5000)
+#     sales = Sale.create!(
+#       [
+#         {proceed: okane, shop_id: 1, created_at: date.since(count.days)}
+#       ]
+#     )
+#     count += 1
+# end
+item = Item.last
+order = Order.create!(
+   [
+     {shop_id: 1, item_id: item.id, name: item.name, amount: 2, total_price: (item.price * 2), status: "in_order", created_at: "2023/1/7"}
+   ]
+ )
